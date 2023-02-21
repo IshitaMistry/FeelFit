2376826748ada1515717bb6a6d2d75bf2f096aa8
IshitaMistry <ishitamistry.mca21@scet.ac.in>
2023-02-21 16:57:21 +0530

    initial commit

---
+++ app/src/main/java/com/example/feelfit/ProximitySensor.kt
@@ -0,0 +1,76 @@
+package com.example.feelfit
+
+import android.R.bool
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+
+class ProximitySensor : AppCompatActivity() {
+
+    lateinit var sensorStatusTV: TextView
+    lateinit var proximitySensor: Sensor
+    lateinit var sensorManager: SensorManager
+    var hasDisplayedToast: Boolean =false
+
+    var count = 0;
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_proximity_sensor)
+
+
+        sensorStatusTV = findViewById(R.id.idTVSensorStatus)
+        // on below line we are initializing our sensor manager
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        // on below line we are initializing our proximity sensor variable
+        proximitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY)
+        // on below line we are checking if the proximity sensor is null
+        if (proximitySensor == null) {
+            // on below line we are displaying a toast if no sensor is available
+            Toast.makeText(this, "No proximity sensor found in device..", Toast.LENGTH_SHORT).show()
+            finish()
+        } else {
+            // on below line we are registering
+            // our sensor with sensor manager
+            sensorManager.registerListener(
+                proximitySensorEventListener,
+                proximitySensor,
+                SensorManager.SENSOR_DELAY_NORMAL
+            )
+        }
+    }
+
+    // calling the sensor event class to detect
+    // the change in data when sensor starts working.
+    var proximitySensorEventListener: SensorEventListener? = object : SensorEventListener {
+        override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {
+            // method to check accuracy changed in sensor.
+        }
+
+        override fun onSensorChanged(event: SensorEvent) {
+            // check if the sensor type is proximity sensor.
+            if (event.sensor.type == Sensor.TYPE_PROXIMITY) {
+                //if (event.values[0] == 0f) {
+
+                if(event.values[0] == 0f&& !hasDisplayedToast) {
+
+                    var C = count++
+
+                    // here we are setting our status to our textview..
+                    // if sensor event return 0 then object is closed
+                    // to sensor else object is away from sensor.
+                    sensorStatusTV.text = C.toString()
+                } else {
+                    // on below line we are setting text for text view
+                    // as object is away from sensor.
+                }
+            }
+        }
+    }
+    }
