dae5dc41c96a856e864f32fb15264bdd2fae8bbe
IshitaMistry <ishitamistry.mca21@scet.ac.in>
2023-02-22 16:31:30 +0530

    till diet plan with fragments

--- app/src/main/java/com/example/feelfit/DietPlanOne.kt
+++ app/src/main/java/com/example/feelfit/DietPlanOne.kt
@@ -2,10 +2,47 @@ package com.example.feelfit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
 class DietPlanOne : AppCompatActivity() {
+
+
+    var tabLayout: TabLayout? = null
+    var viewPager: ViewPager? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_diet_plan_one)
+
+        tabLayout = findViewById(R.id.tablelayout)
+        viewPager = findViewById(R.id.viewpage)
+
+        tabLayout!!.addTab(tabLayout!!.newTab().setText("Veg"))
+        tabLayout!!.addTab(tabLayout!!.newTab().setText("Non-Veg"))
+        tabLayout!!.tabGravity = TabLayout.GRAVITY_FILL
+
+        val adapter = AdapterDiet(this,supportFragmentManager,tabLayout!!.tabCount)
+        viewPager!!.adapter = adapter
+
+        viewPager!!.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tabLayout))
+
+
+        tabLayout!!.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab) {
+                viewPager!!.currentItem = tab.position
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab) {
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+            }
+
+
+        }
+        )
+
+
+
     }
 }
