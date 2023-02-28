15b6011f929a41f5085a4e2d8cae38fc82a1937c
IshitaMistry <ishitamistry.mca21@scet.ac.in>
2023-02-28 11:35:02 +0530

    till normal activity UI

--- app/src/main/java/com/example/feelfit/NormalExercise/Normal2.kt
+++ app/src/main/java/com/example/feelfit/NormalExercise/Normal2.kt
@@ -1,12 +1,32 @@
 package com.example.feelfit.NormalExercise
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.example.feelfit.DietPlan.DietPlanOne
 import com.example.feelfit.R
+import com.example.feelfit.timer
 
 class Normal2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_normal2)
+
+
+        val Timern2 = findViewById<Button>(R.id.timern2)
+        Timern2.setOnClickListener {
+            intent = Intent(this,timer::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+
+        val dietnnn = findViewById<Button>(R.id.dietn2)
+        dietnnn.setOnClickListener {
+            intent = Intent(this, DietPlanOne::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
