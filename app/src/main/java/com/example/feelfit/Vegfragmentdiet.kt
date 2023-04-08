bb1143c0e02d718b8c69de19759a645e55756ed5
diwakar <shubhudiws223@gmail.com>
2023-04-08 15:27:02 +0530

    only veg fragment

--- app/src/main/java/com/example/feelfit/Vegfragmentdiet.kt
+++ app/src/main/java/com/example/feelfit/Vegfragmentdiet.kt
@@ -1,59 +1,61 @@
 package com.example.feelfit
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.firebase.ui.database.FirebaseRecyclerOptions
+import com.google.firebase.database.FirebaseDatabase
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [VegfragmentDiet.newInstance] factory method to
- * create an instance of this fragment.
- */
 class VegfragmentDiet : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: MyFirebaseRecyclerAdapter
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_vegdiet, container, false)
+        val view= inflater.inflate(R.layout.fragment_vegdiet, container, false)
+
+        recyclerView = view.findViewById(R.id.RecyclerViewDietFragment)
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        // Create the FirebaseRecyclerOptions object
+        val ref = FirebaseDatabase.getInstance().getReference("Diet").child("Veg")
+        Log.e("PIPI", "onCreateView: $ref", )
+        val options = FirebaseRecyclerOptions.Builder<VegDModel>()
+            .setQuery(ref, VegDModel::class.java)
+            .build()
+        Log.e("kkkkk", "onCreateView: ${options.snapshots}")
+
+
+        // Create the adapter
+        adapter = MyFirebaseRecyclerAdapter(options)
+        // Set the adapter to the RecyclerView
+        recyclerView.adapter = adapter
+
+        return view
+    }
+    override fun onStart() {
+        super.onStart()
+        // Start listening for changes to the data
+        adapter.startListening()
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment VegfragmentDiet.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            VegfragmentDiet().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onStop() {
+        super.onStop()
+        // Stop listening for changes to the data
+        adapter.stopListening()
     }
+
+
+
+
 }
